1d8c791a90c2d54eb90d8f3071589af6f6a0e957
hoangtv49 <codyhoang.it@gmail.com>
2024-03-17 22:39:39 +0700

    feat: add redis strings json and list

--- .sh
+++ .sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm --name redis -p 6379:6379 -v $(pwd)/redis-data:/data redis --save 60 1
+docker run -it --rm --name redis -p 6379:6379 redis --save 60 1
--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,30 @@ import { createClient } from 'redis';
   const client = await createClient().connect();
   console.log('connected to redis!');
 
-  await client.set('key', 'value');
-  const value = await client.get('key');
-  console.log(value);
+  // Data Types
+  // String
+  await client.set('string', 'hello redis');
+
+  // JSON
+  await client.set(
+    'json',
+    JSON.stringify({ learner: 'Hoang', subject: 'redis', score: 10 })
+  );
+
+  // Lists
+  // Act as queue
+  const res1 = await client.lPush('bikes:repairs', 'bike:1');
+  const res2 = await client.lPush('bikes:repairs', 'bike:2');
+
+  // console.log(await client.rPop('bikes:repairs'));
+  // console.log(await client.rPop('bikes:repairs'));
+
+  // Act as stack
+  // console.log(await client.lPop('bikes:repairs'));
+  // console.log(await client.lPop('bikes:repairs'));
+
+  // Length of list
+  console.log(await client.lLen('bikes:repairs'));
 
   await client.disconnect();
   console.log('disconnected from redis!');
